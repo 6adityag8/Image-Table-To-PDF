300f671495acaa4cf83cdf3ccb6393f56f21c7dd
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-12 23:33:53 +0530

    Add 'home' app to render the index page

--- image_table_to_pdf/urls.py
+++ image_table_to_pdf/urls.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 from api import urls as api_url
+from home import urls as home_url
 
 urlpatterns = [
-    path('api/', include(api_url)),
-]
+                  path('', include(home_url)),
+                  path('api/', include(api_url)),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
