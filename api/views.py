e4df811e1da675b186a2c9800e93cb495f4791f9
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-14 13:46:00 +0530

    Change the error status code

--- api/views.py
+++ api/views.py
@@ -1,12 +1,8 @@
-import logging
-
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .utils import create_pdf_file
-
-logger = logging.getLogger('api_logger')
 
 
 @api_view(['POST'])
@@ -15,5 +11,4 @@ def create_pdf_api(request):
         pdf_path = create_pdf_file(request.data)
         return Response({'created_pdf': pdf_path}, status=status.HTTP_200_OK)
     except Exception as e:
-        logger.exception(e)
-        return Response({'error': e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({'error': str(e)}, status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
