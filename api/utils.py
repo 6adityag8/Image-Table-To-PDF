4694df42e2bd5a3c09b2d7b7ccb7d387fa952071
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-14 23:46:48 +0530

    Refactor image upload api

--- api/utils.py
+++ api/utils.py
@@ -34,3 +34,27 @@ def create_pdf_file(data):
     pdfkit.from_string(html_string, pdf_file_path, css=os.path.join(settings.STATIC_ROOT, 'home/css/index.css'))
 
     return file_name
+
+
+def save_uploaded_file(image_file):
+    """
+    Saves the uploaded image in MEDIA_ROOT.
+    :param image_file: file object received from frontend through POST request
+    :return: path of the saved file
+    """
+    # Creates the MEDIA ROOT directory if it doesn't exist
+    if not os.path.exists(settings.MEDIA_ROOT):
+        os.mkdir(settings.MEDIA_ROOT)
+
+    # Getting the path where the uploaded file will be saved
+    image_path = os.path.join(settings.MEDIA_ROOT, image_file.name)
+
+    with open(image_path, 'wb+') as destination:
+        # handle the case when size of the image is over than 2.5 Mb
+        if image_file.multiple_chunks:
+            for chunk in image_file.chunks():
+                destination.write(chunk)
+        else:
+            destination.write(image_file.read())
+
+    return image_path
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from .views import index, upload_image
+from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
-    path('upload_image/', upload_image, name='upload_image'),
 ]
