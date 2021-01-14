c8bc45ed48de672fc75750c573c7e6aa04fb5214
Aditya Gupta <adityag@mindfiresolutions.com>
2021-01-14 15:13:33 +0530

    Load wkhtmltopdf path on heroku and pass it as config to pdfkit

--- api/utils.py
+++ api/utils.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 
 import pdfkit
 from django.conf import settings
@@ -31,6 +32,15 @@ def create_pdf_file(data):
     # Getting the path where the uploaded file will be saved
     pdf_file_path = os.path.join(settings.MEDIA_ROOT, file_name)
 
-    pdfkit.from_string(html_string, pdf_file_path)
+    # loading wkhtmltopdf path on heroku and passing it as config to pdfkit
+    if 'DYNO' in os.environ:
+        WKHTMLTOPDF_CMD = subprocess.Popen(
+            ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')],
+            # we default to 'wkhtmltopdf' as the binary name
+            stdout=subprocess.PIPE).communicate()[0].strip()
+        config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
+        pdfkit.from_string(html_string, pdf_file_path, configuration=config)
+    else:
+        pdfkit.from_string(html_string, pdf_file_path)
 
     return file_name
